5229e2b6b01827bff95b917ef59f67b280954274
Jviguy <jviguytwo2@gmail.com>
2025-11-03 00:38:30 -0500

    rust bins/ each module is its own seperate crate so thats fucked.

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,4 +1,4 @@
-use crate::aoc_main;
+use {{project-name}}::aoc_main;
 
 fn part1(input: &str) -> Result<String, Box<dyn std::error::Error>> {
     Ok(format!("Input has {} lines", input.lines().count()))
