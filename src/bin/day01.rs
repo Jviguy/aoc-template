4a76c2957c52b3c64425d13c213a31ca8d045304
Jviguy <jviguytwo2@gmail.com>
2025-11-03 00:02:38 -0500

    change it so that we use crate:: instead of replacing the project name

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,4 +1,4 @@
-use {{project_name}}::aoc_main;
+use crate::aoc_main;
 
 fn part1(input: &str) -> Result<String, Box<dyn std::error::Error>> {
     Ok(format!("Input has {} lines", input.lines().count()))
